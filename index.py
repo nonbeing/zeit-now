cd6e9e3364836df270a138e017841b3d086c5dc3
Ambar <anattaspace@gmail.com>
2019-05-17 10:24:45 +0530

    new test

--- index.py
+++ index.py
@@ -6,5 +6,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(str("Hello from Now 2.0 on Python!").encode())
+        self.wfile.write(str("Hello from Now 2.0 on Python!<br/><h1>new test</h1>").encode())
         return
